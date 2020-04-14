f633ada8840d51ba1bb4998d8514f6a1b59ab830
pblankley <31221512+pblankley@users.noreply.github.com>
2020-04-14 17:16:31 -0400

    polish pandas bug report

--- pandas_agg_vs_mean/pandas_test.py
+++ pandas_agg_vs_mean/pandas_test.py
@@ -1,14 +1,15 @@
 import pandas as pd
+import numpy as np
 
-data_url = 'https://drive.google.com/open?id=1j66Z6bYrlRN6jbvhko6zqR99uBfS3blO'
-data = pd.read_csv(data_url)
-print(data)
-using_agg = data.groupby('Campaign ID').resample('Y', on='other date').agg({'Click Cost': 'mean'})
-using_mean = data.groupby('Campaign ID').resample('Y', on='other date').mean()
-diff = using_agg['Click Cost'] == using_mean['Click Cost']
+data = pd.DataFrame({
+    'cat': ['cat_1', 'cat_1', 'cat_2', 'cat_1', 'cat_2', 'cat_1', 'cat_2', 'cat_1'],
+    'num': [5,20,22,3,4,30,10,50],
+    'date': ['2019-2-1', '2018-02-03','2020-3-11','2019-2-2', '2019-2-2', '2018-12-4','2020-3-11', '2020-12-12']
+})
+data['date'] = pd.to_datetime(data['date'])
+using_agg = data.groupby('cat').resample('Y', on='date').agg({'num': 'mean'})
+using_mean = data.groupby('cat').resample('Y', on='date').mean()
 
-print(diff)
-
-# assert(np.allclose(using_agg['Click Cost'], using_mean['Click Cost']))
+assert(np.allclose(using_agg['num'], using_mean['num']))
 
 print(pd.show_versions())
